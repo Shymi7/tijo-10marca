ca3627d388013da63ed258f48297628fdc3dceef
Szymon Grzeszczuk <szymon3520@gmail.com>
2023-03-16 13:11:59 +0100

    sumOfGivenNNumbers function added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println(sumOfGivenNNumbers(7));
+
+    }
+
+    public static int sumOfGivenNNumbers(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
